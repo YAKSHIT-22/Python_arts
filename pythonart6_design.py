565a44303ebe81a4b33cc7055fce0dfb0e5add12
Yashika <yashika1586.be21@chitkara.edu.in>
2022-05-12 13:58:00 +0530

    Initial Commit

---
+++ pythonart6_design.py
@@ -0,0 +1,18 @@
+
+   
+import turtle
+
+t = turtle.Turtle()
+S = turtle.Screen()
+
+S.bgcolor("black")
+t.speed(0)
+color = ("yellow","red","pink","cyan","green","blue","white","grey","purple")
+
+for i in range(200):
+    t.pencolor(color[i%6])
+    t.circle(190-i/2,90)
+    t.lt(90)
+    t.circle(190-i/3,90)
+    t.lt(60)
+S.exitonclick()
